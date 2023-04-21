CNN: fix gradient not being saved during update steps

algos/test_cnn/RADTEAM_core.py
@@ -1395,7 +1395,6 @@ class EmptyCritic:
         return True
 
 
-# TODO Add to new map
 # Developed from RAD-A2C https://github.com/peproctor/radiation_ppo
 class PFRNNBaseCell(nn.Module):
     """Parent class for Particle Filter Recurrent Neural Networks"""
@@ -1511,7 +1510,6 @@ class PFRNNBaseCell(nn.Module):
         return mean + eps * std
 
 
-# TODO Add to new map
 # Developed from RAD-A2C https://github.com/peproctor/radiation_ppo
 class PFGRUCell(PFRNNBaseCell):
     """Particle Filter Gated Recurrent Unit"""
@@ -1952,6 +1950,42 @@ class CNNBase:
     ) -> Tuple[ActionChoice, HeatMaps]:
         """Alias for select_action"""
         return self.select_action(state_observation=state_observation, id=self.id, hidden=hidden)
+
+    def step_with_gradient(
+        self, 
+        id: int,
+        state_observation: Dict[int, npt.NDArray], 
+        action_taken: torch.Tensor,
+        hidden: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Identical to select_action, however stores the gradient for a gradient update and returns different information.
+
+        :param state_observation: (Dict[int, npt.NDArray]) Dictionary with each agent's observation. The agent id is the key.
+        :param id: (int) ID of the agent who's observation is being processed. This allows any agent to recreate mapbuffers for any other agent
+        :param hidden: hidden layers for PFGRU.
+        """
+        
+        with torch.no_grad():
+            obs_list = [state_observation[i][:3] for i in range(self.number_of_agents)] # Create a list of just readings and locations for all agents
+            obs_tensor = torch.as_tensor(obs_list, dtype=torch.float32)
+
+            location_prediction, new_hidden = self.model(obs_tensor, hidden)
+            
+            # Process data and create maps
+            batched_actor_mapstack, batched_critic_mapstack = self.get_map_stack(
+                id = id,
+                state_observation = state_observation,
+                location_prediction = tuple(location_prediction.tolist())
+            )
+
+        # Get action logprobs and entroy WITH gradient
+        action_logprobs, dist_entropy = self.pi.get_action_information(state_map_stack=batched_actor_mapstack, action=action_taken)
+
+        state_value: torch.Tensor = self.critic.forward(batched_critic_mapstack)
+        
+        return action_logprobs, dist_entropy, state_value
+
 
     def reset_hidden(self, batch_size=1) -> Tuple[torch.Tensor, torch.Tensor]:
         """For compatibility - returns nothing"""

algos/test_cnn/train.py
@@ -233,7 +233,7 @@ def ppo(env_fn, actor_critic=core.CNNBase, ac_kwargs=dict(), seed=0,
     if proc_id() == 0:
         print(f'Local steps per epoch: {local_steps_per_epoch}')
 
-    def update_a2c(data, env_sim, minibatch=None,iter=None):
+    def update_a2c(id, data, env_sim, minibatch=None, iter=None):
         observation_idx = 11
         action_idx = 14
         logp_old_idx = 13
@@ -242,9 +242,21 @@ def ppo(env_fn, actor_critic=core.CNNBase, ac_kwargs=dict(), seed=0,
         source_loc_idx = 15
         
         ep_form= data['ep_form']
+        pi_ep_form = data['actor_heatmaps_ep_form']
+        v_ep_form = data['critic_heatmaps_ep_form']
+        
+        assert len(ep_form) == len(pi_ep_form)
+        assert len(ep_form) == len(v_ep_form)
+        
         pi_info = dict(kl=[], ent=[], cf=[], val= np.array([]), val_loss = [])
+        
+        # Randomly sample epsiodes
         ep_select = np.random.choice(np.arange(0,len(ep_form)),size=int(minibatch),replace=False)
         ep_form = [ep_form[idx] for idx in ep_select]
+        pi_ep_form = [pi_ep_form[idx] for idx in ep_select]
+        v_ep_form = [v_ep_form[idx] for idx in ep_select]
+        
+        # Storage and buffers
         loss_sto = torch.zeros((len(ep_form),4),dtype=torch.float32)
         loss_arr_buff = torch.zeros((len(ep_form),1),dtype=torch.float32)
         loss_arr = torch.autograd.Variable(loss_arr_buff)
@@ -253,11 +265,21 @@ def ppo(env_fn, actor_critic=core.CNNBase, ac_kwargs=dict(), seed=0,
             #For each set of episodes per process from an epoch, compute loss 
             trajectories = ep[0]
             
-            hidden = ac.reset_hidden()
-            obs, act, logp_old, adv, ret, src_tar = trajectories[:,:observation_idx], trajectories[:,action_idx],trajectories[:,logp_old_idx], \
-                                                     trajectories[:,advantage_idx], trajectories[:,return_idx,None], trajectories[:,source_loc_idx:].clone()
-            #Calculate new log prob.
-            pi, val, logp, loc = ac.grad_step(obs, act, hidden=hidden)
+            ac.reset() # Clear stored maps
+            hidden = ac.reset_hidden() # Get hidden layers for pfgru
+            
+            obs = trajectories[:,:observation_idx]
+            act = trajectories[:,action_idx]
+            logp_old = trajectories[:,logp_old_idx]
+            adv = trajectories[:,advantage_idx]
+            ret = trajectories[:,return_idx,None]
+            src_tar = trajectories[:,source_loc_idx:].clone()
+            
+            # Calculate new log prob.
+            #pi, val, logp, loc = ac.step_with_gradient(obs, act, hidden=hidden)
+            pi, val, logp, loc = ac.step_with_gradient(obs, act, hidden=hidden)
+            
+            # PPO-Clip starts here
             logp_diff = logp_old - logp 
             ratio = torch.exp(logp - logp_old)
 
@@ -410,7 +432,7 @@ def ppo(env_fn, actor_critic=core.CNNBase, ac_kwargs=dict(), seed=0,
             
         while (not term and kk < train_pi_iters):
             #Early stop training if KL-div above certain threshold
-            pi_l, pi_info, term, loc_loss = update_a2c(data, env, minibatch=min_iters,iter=kk)
+            pi_l, pi_info, term, loc_loss = update_a2c(id=0, data=data, env_sim=env, minibatch=min_iters, iter=kk)
             kk += 1              
         
         #Reduce learning rate
@@ -520,6 +542,7 @@ def ppo(env_fn, actor_critic=core.CNNBase, ac_kwargs=dict(), seed=0,
                 if not env.epoch_end:
                     #Reset detector position and episode tracking
                     hidden = ac.reset_hidden()  
+                    ac.reset()
                         
                     o, _, _, _ = env.reset()
                     o = o[0]